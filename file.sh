162758794237797baa28c71ba0a2aea54dc86a8f
sahu <cpsahu19@gmail.com>
2023-02-16 14:46:48 +0530

    updated shell script

--- file.sh
+++ file.sh
@@ -1,12 +1,11 @@
 echo "this is shellscripting learning phase"
 ls
-sleep 3
 pwd
 sleep 4
-sudo apt-get update -y
+sudo apt update -y
 sleep 5
 ls
-sudo apt-get install -y
+sudo apt-get install -y jenkins
 ls
 date
 echo "this last one commit is from git server"
